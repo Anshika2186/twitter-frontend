329b0584fe250ee15c03787f89bf6f4298504a84
Anshika2186 <97463716+Anshika2186@users.noreply.github.com>
2024-10-15 18:32:43 +0530

    Update HomeBody.js

--- src/components/HomeBody.js
+++ src/components/HomeBody.js
@@ -46,7 +46,7 @@ function HomeBody() {
       password: password,
     };
 
-    fetch("http://localhost:5000/", {
+    fetch("https://twitter-backend-gsqq.onrender.com/", {
       method: "POST",
       headers: { "Content-type": "application/json" },
       body: JSON.stringify(oldPerson),
@@ -105,7 +105,7 @@ function HomeBody() {
 
         <form
           className="homeContainer-form"
-          action="http://localhost:5000/signup"
+          action="https://twitter-backend-gsqq.onrender.com/signup"
           method="post"
           onSubmit={handleSubmit}
         >
